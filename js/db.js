ab259d03c63d8cd96c6c27be0da68b4ec44c8577
Calamarneitor777 <santiagogoku443@gmail.com>
2026-06-10 21:21:30 -0600

    agregamos boton de nuevo platillo

--- js/db.js
+++ js/db.js
@@ -1,6 +1,35 @@
 db.collection("platillo").onSnapshot((datos) => {
-   datos.forEach((registro) => {
-    console.log(registro);
-    
-   });
+    datos.docChanges().forEach((registro) => {
+      if(registro.type === "added"){
+        mostrarPlatillo(registro.doc.data(),registro.doc.id);
+      }
+      if (registro.type === "modified"){
+         actualizarPlatillo(registro.doc.data(),registro.doc.id);
+      }
+      
+
+    });
+});
+
+const formularioAgregar = document.querySelector("form");
+formularioAgregar.addEventListener("submit",(e)=>{
+   e.preventDefault();
+   const platilloNuevo = {
+      nombre:formularioAgregar.title.value,
+      ingredientes:formularioAgregar.ingredients.value,
+      precio:formularioAgregar.precio.value
+}
+
+   db.collection("platillo").add(platilloNuevo)
+   .catch((error) => {
+      console.log(error);
+      alert("Error al agregar platillo")
+   }
+
+   )
+
+   formularioAgregar.title.value="";
+   formularioAgregar.ingredients.value="";
+   formularioAgregar.precio.value="";
+   alert("Platillo agregado");
 });
--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 btnAgregarPlatillo = document.getElementById('btnAgregarPlatillo');
-
+let contenido ="";
 
 document.addEventListener('DOMContentLoaded', function() {
   // nav menu
@@ -13,3 +13,34 @@ document.addEventListener('DOMContentLoaded', function() {
 btnAgregarPlatillo.addEventListener('click', function() {
   alert('Platillo agregado');
 });
+
+function mostrarPlatillo(platillo, id){
+ contenido += 
+  `<div class="card-panel recipe white row" 
+  id="${id}"> 
+    <div class= "recipe-details">
+      <div class= "recipe-title">
+         Nombre: ${platillo.nombre}
+      </div>
+      <div class="recipe-ingredients">
+  Ingredientes: ${platillo.ingredientes}
+      </div>
+      <div class="recipe-price">
+  Precio: $${platillo.precio}
+      </div> 
+    </div>
+      <div class="recipe-delete">
+        <i class="material-icons" data-id="${id}">delete_outline</i>
+      </div>
+
+  </div>`;
+document.querySelector('.recipes').innerHTML = contenido
+}
+
+
+function actualizarPlatillo(platillo, id){
+  let tarjeta =document.getElementById(`${id}`);
+  tarjeta.querySelector(".recipe-title").innerHTML = platillo.nombre;
+  tarjeta.querySelector(".recipe-ingredients").innerHTML = platillo.ingredientes;
+    tarjeta.querySelector(".recipe-price").innerHTML = `$${platillo.precio}`;
+}
