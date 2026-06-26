e5e1c0f18e2a462096adc815fcc09c38cbefd685
Calamarneitor777 <santiagogoku443@gmail.com>
2026-06-25 21:22:41 -0600

    se hizo el changes

--- js/pedidos.js
+++ js/pedidos.js
@@ -1,9 +1,32 @@
-let contenidoLista='';
-function AgregarALista(platillo, id){
-    contenidoLista = `<option value='${id}'>
-    ${mostrarPlatillo.nombre}
-    </option>`;
-    document.getElementById('listaPlatillos').innerHTML=
-    contenidoLista;
+let contenidoLista = '';
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    const menus = document.querySelectorAll('.side-menu');
+    M.Sidenav.init(menus, {edge: 'right'});
+})
+
+
+let contenidoLista ='';
+
+db.collection("platillo").onSnapshot((datos) => {
+  datos.docChanges().forEach((registro) => {
+    if (registro.type === "added"){
+      agregarALista(registro.doc.data(),registro.doc.id);
+    }
+    
+  });
+  var elems = document.querySelectorAll('select');
+
+})
+
+
+function agregarALista(platillo, id){
+contenidoLista = `<option value=''>
+${platillo.nombre} </option>`;
+document.getElementById ('listaPlatillo').innerHTML=contenidoLista;
+
 
 }
+
+M.AutoInit();
